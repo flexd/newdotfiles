65b08c4d8835b0cbdeaf0266e8ff2b8e8e529146
Kristoffer Berdal <kristoffer.berdal@teledyneflir.com>
2023-11-02 12:05:46 +0100

    add neovim config

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,166 @@
+-------------------- HELPERS -------------------------------
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local opt = vim.opt  -- to set options
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-------------------- PLUGINS -------------------------------
+cmd 'packadd paq-nvim'               -- load the package manager
+local paq = require('paq').paq  -- a convenient alias
+paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
+paq {'drewtempelmeyer/palenight.vim'}
+--paq {'shougo/deoplete-lsp'}
+--paq {'shougo/deoplete.nvim', run = fn['remote#host#UpdateRemotePlugins']}
+paq {'nvim-treesitter/nvim-treesitter'}
+paq {'p00f/clangd_extensions.nvim'}
+paq {'neovim/nvim-lspconfig'}
+paq {'junegunn/fzf', run = fn['fzf#install']}
+paq {'junegunn/fzf.vim'}
+paq {'ojroques/nvim-lspfuzzy'}
+paq {'nvim-lua/plenary.nvim'}
+paq {'lewis6991/gitsigns.nvim'}
+paq {'navarasu/onedark.nvim'}
+paq {'tpope/vim-fugitive'}
+paq {'preservim/nerdcommenter'}
+paq {'hrsh7th/nvim-cmp'} -- Autocompletion plugin
+paq {'hrsh7th/cmp-nvim-lsp'}
+paq {'saadparwaiz1/cmp_luasnip'}
+paq {'L3MON4D3/LuaSnip'} -- Snippets plugin
+paq {'arcticicestudio/nord-vim'}
+paq {'rafamadriz/friendly-snippets'}
+paq {'peterhoeg/vim-qml'}
+--g['deoplete#enable_at_startup'] = 1  -- enable deoplete at startup
+
+-------------------- OPTIONS -------------------------------
+opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options (for deoplete)
+opt.expandtab = true                -- Use spaces instead of tabs
+opt.hidden = true                   -- Enable background buffers
+opt.ignorecase = true               -- Ignore case
+opt.joinspaces = false              -- No double spaces with join
+opt.list = true                     -- Show some invisible characters
+opt.number = true                   -- Show line numbers
+opt.relativenumber = true           -- Relative line numbers
+opt.scrolloff = 4                   -- Lines of context
+opt.shiftround = true               -- Round indent
+opt.shiftwidth = 4                  -- Size of an indent
+opt.sidescrolloff = 8               -- Columns of context
+opt.smartcase = true                -- Do not ignore case with capitals
+opt.smartindent = true              -- Insert indents automatically
+opt.splitbelow = true               -- Put new windows below current
+opt.splitright = true               -- Put new windows right of current
+opt.tabstop = 4                     -- Number of spaces tabs count for
+opt.termguicolors = true            -- True color support
+opt.wildmode = {'list', 'longest'}  -- Command-line completion mode
+opt.wrap = false                    -- Disable line wrap
+
+vim.g.mapleader = ","
+
+-------------------- MAPPINGS ------------------------------
+vim.g['fzf_action'] = {['ctrl-s'] = 'split', ['ctrl-v'] = 'vsplit'}
+map('', '<leader>c', '"+y')       -- Copy to clipboard in normal, visual, select and operator modes
+map('', '<leader>f', '<cmd>:FZF<CR>')
+map('', '<leader>g', '<cmd>:GFiles<CR>')
+map('i', '<C-u>', '<C-g>u<C-u>')  -- Make <C-u> undo-friendly
+map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
+
+-- <Tab> to navigate the completion menu
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+
+map('n', '<C-l>', '<cmd>noh<CR>')    -- Clear highlights
+map('n', '<leader>o', 'm`o<Esc>``')  -- Insert a newline in normal mode
+
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+local bufopts = { noremap=true, silent=true, buffer=bufnr }
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+map('n', '<leader>gre', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<leader>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+-------------------- TREE-SITTER ---------------------------
+local ts = require 'nvim-treesitter.configs'
+ts.setup {ensure_installed = 'cpp', highlight = {enable = true}}
+
+-------------------- LSP -----------------------------------
+local lsp = require 'lspconfig'
+local lspfuzzy = require 'lspfuzzy'
+
+-- We use the default settings for ccls and pylsp: the option table can stay empty
+--lsp.clangd.setup {}
+require("clangd_extensions").setup()
+lsp.pylsp.setup {}
+lspfuzzy.setup {}  -- Make the LSP client use FZF instead of the quickfix list
+
+-- gitsigns.nvim
+local gitsigns = require('gitsigns')
+gitsigns.setup {
+  signs = {
+    add = {text = '+'},
+    change = {text = '~'},
+    delete = {text = '-'}, topdelete = {text = '-'}, changedelete = {text = '≃'},
+  },
+}
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+-------------------- COMMANDS ------------------------------
+cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- disabled in visual mode
+cmd 'colorscheme selenized'            -- Put your favorite colorscheme here
+require("luasnip.loaders.from_vscode").lazy_load()
+
+vim.opt.mouse = ""
